c6ab7c0ec8aef439fc69df96bcd7136531859051
sanjit <sanjitkumarsingh4@gmail.com>
2018-02-14 01:21:45 +0530

    Adding Project to the Repository

---
+++ DummyWebApiUpload/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DummyWebApiUpload.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DummyWebApiUpload/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DummyWebApiUpload.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
